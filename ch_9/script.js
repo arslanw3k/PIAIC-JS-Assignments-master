c07ef74ff2bf0d236efa324f44401e4e120e103f
arslanw3k <65291675+arslanw3k@users.noreply.github.com>
2021-02-04 19:35:49 +0300

    Add files via upload

--- ch_9/script.js
+++ ch_9/script.js
@@ -1,4 +1,4 @@
-var fname = "Arslan"; 
-var lname = "Butt,";
-var age = 38;
-alert("Hello"+ "  " +fname +" "+lname +" "+"your are"+" "+ age+" "+ "years old"); 
+var fname = prompt("Your First Name") ; 
+var lname = prompt("Your Last Name") ;
+var age = prompt("Your age") ;
+alert("Hello"+ "  " +fname +" "+lname +" "+"your are"+" "+ age+" "+ "years old");
