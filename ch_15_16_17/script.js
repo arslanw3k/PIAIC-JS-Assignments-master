3e4f33c48ff4ca6bafd631bdedd9f75569b33b6e
arslanw3k <65291675+arslanw3k@users.noreply.github.com>
2021-02-07 08:23:19 +0300

    Update script.js

--- ch_15_16_17/script.js
+++ ch_15_16_17/script.js
@@ -1,4 +1,8 @@
-var fname = "Arslan"; 
-var lname = "Butt,";
-var age = 38;
-alert("Hello"+ "  " +fname +" "+lname +" "+"your are"+" "+ age+" "+ "years old"); 
+var courses = ["CNC","BC","IOT","AI","5G"];
+alert("Old list of Courses" +" "+courses);
+var courses1 = courses.splice(0,1,prompt("Change Course1 (CNC) Name"));
+var courses2 = courses.splice(1,1,prompt("Change Course2 (BC) Name"));
+var courses3 = courses.splice(2,1,prompt("Change Course3 (IOT) Name"));
+var courses4 = courses.splice(3,1,prompt("Change Course4 (AI) Name"));
+var courses5 = courses.splice(4,1,prompt("Change Course5 (5G) Name"));
+alert("New list of Courses" +" "+courses);
