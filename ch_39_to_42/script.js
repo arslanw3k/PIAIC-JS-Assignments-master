04dd873f59079829fc3c1de96d1a87a591abd580
arslanw3k <65291675+arslanw3k@users.noreply.github.com>
2021-02-19 20:54:39 +0300

    Add files via upload

--- ch_39_to_42/script.js
+++ ch_39_to_42/script.js
@@ -1,52 +1,38 @@
 
-        
-        var course1Marks = parseFloat(prompt("Enter Course-1 Marks : "));
-        var course2Marks = parseFloat(prompt("Enter Course-2 Marks : "));
-        var course3Marks = parseFloat(prompt("Enter Course-3 Marks : "));
-        var course4Marks = parseFloat(prompt("Enter Course-4 Marks : "));
-        var course5Marks = parseFloat(prompt("Enter Course-5 Marks : "));
-        
-        var obtainedMarks = course1Marks + course2Marks + course3Marks + course4Marks + course5Marks;
+var c1 = parseInt(prompt("Enter Course 1 marks")); 
+var c2 = parseInt(prompt("Enter Course 2 marks")); 
+var c3 = parseInt(prompt("Enter Course 3 marks")); 
+var c4 = parseInt(prompt("Enter Course 4 marks")); 
+var c5 = parseInt(prompt("Enter Course 5 marks")); 
+// ts = Total Score
+var ts = 500;
+ // tso = Total Score Obtained  
+var tso = c1+c2+c3+c4+c5;
+//  p = Percent Achieved   
+var p = parseFloat((tso*100)/ts);
 
-        var totalMarks = 500;
-
-        var percentage = parseFloat((obtainedMarks * 100) / totalMarks);
-
-              switch (true) {
-            
-                       case percentage >= 90 && percentage <= 100:
-                alert(`Total Obtained Marks : ${obtainedMarks} , Percentage : ${percentage}% , Grade : A+`);
-                break;
-
-           
-            case percentage >= 75 && percentage <= 89:
-                alert(`Total Obtained Marks : ${obtainedMarks} , Percentage : ${percentage}% , Grade : A`);
-                break;
-
-           
-            case percentage >= 60 && percentage <= 74:
-                alert(`Total Obtained Marks : ${obtainedMarks} , Percentage : ${percentage}% , Grade : B`);
-                break;
-
-          
-            case percentage >= 45 && percentage <= 59:
-                alert(`Total Obtained Marks : ${obtainedMarks} , Percentage : ${percentage}% , Grade : C`);
-                break;
-
-            
-            case percentage >= 30 && percentage <= 44:
-               alert(`Total Obtained Marks : ${obtainedMarks} , Percentage : ${percentage}% , Grade : D`);
-                break;
-
-            
-            case percentage >= 0 && percentage <= 29:
-                alert(`Total Obtained Marks : ${obtainedMarks} , Percentage : ${percentage}% , Grade : FAIL`);
-                break;
-
-            
-            default:
-                alert("Error! Enter Valid Input");
-                break;
-        }
-
-   
+switch(true){
+ 
+  case p >= 90 && p <= 100 :
+    alert("Scored:"+tso+"   "+ "Percenage Achieved:"+p+"%"+"   "+"Grade A+" );
+    break;
+  case p >= 75 && p <= 89 :
+     alert("Scored:"+tso+"   "+ "Percenage Achieved:"+p+"%"+"   "+"Grade A" );
+    break;
+  case p >= 60 && p <= 74 :
+    alert("Scored:"+tso+"   "+ "Percenage Achieved:"+p+"%"+"   "+"Grade B");
+    break;
+  case p >= 45 &&p <= 60 :
+    alert("Scored:"+tso+"   "+ "Percenage Achieved:"+p+"%"+"   "+"Grade C");
+    break;
+    case p >= 30 && p <= 44 :
+    alert("Scored:"+tso+"   "+ "Percenage Achieved:"+p+"%"+"   "+"Grade D");
+    break;
+    
+case p >= 29 && p <= 0 :
+    alert("Scored:"+tso+"   "+ "Percenage Achieved:"+p+"%"+"   "+"Grade F");
+    break;    
+      
+  default :
+    alert("Please Enter correct values");
+}
